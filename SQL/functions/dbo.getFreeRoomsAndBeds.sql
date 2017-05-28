e4db8b04085791fe260062eb0684178ce1e4115e
Andre Nascimento <andrenascimento@ua.pt>
2017-05-28 20:23:24 +0100

    fix

--- SQL/functions/dbo.getFreeRoomsAndBeds.sql
+++ SQL/functions/dbo.getFreeRoomsAndBeds.sql
@@ -1,15 +1,17 @@
+
 CREATE FUNCTION dbo.getFreeRoomsAndBeds()
 RETURNS TABLE
 AS
 	RETURN(
-		select RoomNumber, BedNumber from exemplo1.BED 
+		select distinct RoomNumber from (select RoomNumber, BedNumber from exemplo1.BED 
 			join exemplo1.BEDROOM on E_RoomNumber = RoomNumber
 
 		except
 		select RoomNumber, BedNumber from exemplo1.PATIENT 
 			join exemplo1.BED on E_BedNumber = BedNumber
-			join exemplo1.BEDROOM on E_RoomNumber = RoomNumber
+			join exemplo1.BEDROOM on E_RoomNumber = RoomNumber) as tmp
 			)
+			
 GO
 -- Execution example
 -- SELECT * FROM dbo.getFreeRoomsAndBeds()
