4e4b804c8414b46ba3abc5e2c9e1ac5ec5993138
Andre Nascimento <andrenascimento@ua.pt>
2017-05-31 23:43:17 +0100

    modifications on newDiagnosed

--- SQL/procedures/dbo.newDiagnosed.sql
+++ SQL/procedures/dbo.newDiagnosed.sql
@@ -2,13 +2,17 @@ CREATE PROCEDURE dbo.newDiagnosed (@E_Name varchar(30), @E_NIF varchar(9), @Seri
 as
 begin
 
-		if  (Exists(Select Name from exemplo1.DISEASE where Name=@E_Name ))
-				if  (Exists(Select NIF from exemplo1.PATIENT where NIF=@E_NIF ))
-					INSERT INTO exemplo1.DIAGNOSED values(
-							@E_Name,
-							@E_NIF,
-							@Seriousness
-					)
+		if  (not Exists(Select Name from exemplo1.DISEASE where Name=@E_Name ))
+			INSERT INTO exemplo1.DISEASE values(
+					@E_Name
+			)
+		if  (Exists(Select NIF from exemplo1.PATIENT where NIF=@E_NIF ))
+			INSERT INTO exemplo1.DIAGNOSED values(
+					@E_Name,
+					@E_NIF,
+					@Seriousness
+			)
 		RETURN
 END
 GO
+
