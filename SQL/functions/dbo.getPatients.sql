c3e65d0952e56073bf60944b0187fb59247a279f
Andre Nascimento <andrenascimento@ua.pt>
2017-06-02 18:27:19 +0100

    mais cenas

--- SQL/functions/dbo.getPatients.sql
+++ SQL/functions/dbo.getPatients.sql
@@ -2,11 +2,11 @@ CREATE FUNCTION dbo.getPatients()
 RETURNS TABLE
 AS
 	RETURN(
-		select exemplo1.PATIENT.NIF, exemplo1.PATIENT.Name, exemplo1.PATIENT.Sex, exemplo1.PATIENT.Phone, exemplo1.PATIENT.Age, exemplo1.PATIENT.Check_in, exemplo1.PATIENT.Check_out, Authorization_to_leave, E_BedNumber, Entry_Date, Exit_Date, RoomNumber, exemplo1.VISITOR.Name as DependentName from exemplo1.PATIENT 
-		join exemplo1.BED on E_BedNumber = BedNumber
-		join exemplo1.BEDROOM on E_RoomNumber = RoomNumber
-		join exemplo1.DEPENDENT on E_PatientNIF = NIF
-		join exemplo1.FAMILY on exemplo1.DEPENDENT.E_CC = exemplo1.FAMILY.E_CC
-		join exemplo1.VISITOR on exemplo1.FAMILY.E_CC = exemplo1.VISITOR.CC
+		select exemplo1.PATIENT.NIF, exemplo1.PATIENT.Name, exemplo1.PATIENT.Sex, exemplo1.PATIENT.Phone, exemplo1.PATIENT.Age, exemplo1.PATIENT.Check_in, exemplo1.PATIENT.Check_out, Authorization_to_leave, E_BedNumber, Entry_Date, Exit_Date, RoomNumber, exemplo1.VISITOR.Name as DependentName, exemplo1.VISITOR.CC as DependentCC, exemplo1.VISITOR.Address as DependentAddress, exemplo1.VISITOR.Phone as DependentPhone  from exemplo1.PATIENT 
+		JOIN exemplo1.BED on E_BedNumber = BedNumber
+		JOIN exemplo1.BEDROOM on E_RoomNumber = RoomNumber
+		FULL OUTER JOIN exemplo1.DEPENDENT on E_PatientNIF = NIF
+		LEFT OUTER JOIN exemplo1.FAMILY on exemplo1.DEPENDENT.E_CC = exemplo1.FAMILY.E_CC
+		LEFT OUTER JOIN exemplo1.VISITOR on exemplo1.FAMILY.E_CC = exemplo1.VISITOR.CC
 		  )		
 GO
