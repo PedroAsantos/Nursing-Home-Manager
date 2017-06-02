ee2bd2a43d571f3fba3bbe3f0038ca582f48d5c4
Andre Nascimento <andrenascimento@ua.pt>
2017-06-02 18:01:14 +0100

    getpatients returns dependent name

--- SQL/functions/dbo.getPatients.sql
+++ SQL/functions/dbo.getPatients.sql
@@ -2,8 +2,11 @@ CREATE FUNCTION dbo.getPatients()
 RETURNS TABLE
 AS
 	RETURN(
-		select NIF, Name, Sex, Phone, Age, Check_in, Check_out, Authorization_to_leave, E_BedNumber, Entry_Date, Exit_Date, RoomNumber from exemplo1.PATIENT 
+		select exemplo1.PATIENT.NIF, exemplo1.PATIENT.Name, exemplo1.PATIENT.Sex, exemplo1.PATIENT.Phone, exemplo1.PATIENT.Age, exemplo1.PATIENT.Check_in, exemplo1.PATIENT.Check_out, Authorization_to_leave, E_BedNumber, Entry_Date, Exit_Date, RoomNumber, exemplo1.VISITOR.Name as DependentName from exemplo1.PATIENT 
 		join exemplo1.BED on E_BedNumber = BedNumber
 		join exemplo1.BEDROOM on E_RoomNumber = RoomNumber
+		join exemplo1.DEPENDENT on E_PatientNIF = NIF
+		join exemplo1.FAMILY on exemplo1.DEPENDENT.E_CC = exemplo1.FAMILY.E_CC
+		join exemplo1.VISITOR on exemplo1.FAMILY.E_CC = exemplo1.VISITOR.CC
 		  )		
 GO
