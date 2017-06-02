914268f3c5d0fc8ba505b586d631672716d77891
Andre Nascimento <andrenascimento@ua.pt>
2017-06-02 19:02:39 +0100

    git cool

--- SQL/functions/dbo.getPatients.sql
+++ SQL/functions/dbo.getPatients.sql
@@ -2,11 +2,7 @@ CREATE FUNCTION dbo.getPatients()
 RETURNS TABLE
 AS
 	RETURN(
-<<<<<<< HEAD
 		select exemplo1.PATIENT.NIF, exemplo1.PATIENT.Name, exemplo1.PATIENT.Sex, exemplo1.PATIENT.Phone, exemplo1.PATIENT.Age, exemplo1.PATIENT.Check_in, exemplo1.PATIENT.Check_out, Authorization_to_leave, E_BedNumber, Entry_Date, Exit_Date, RoomNumber, exemplo1.VISITOR.Name as DependentName, exemplo1.VISITOR.CC as DependentCC, exemplo1.VISITOR.Address as DependentAddress, exemplo1.VISITOR.Phone as DependentPhone, exemplo1.FAMILY.Relationship  from exemplo1.PATIENT 
-=======
-		select exemplo1.PATIENT.NIF, exemplo1.PATIENT.Name, exemplo1.PATIENT.Sex, exemplo1.PATIENT.Phone, exemplo1.PATIENT.Age, exemplo1.PATIENT.Check_in, exemplo1.PATIENT.Check_out, Authorization_to_leave, E_BedNumber, Entry_Date, Exit_Date, RoomNumber, exemplo1.VISITOR.Name as DependentName, exemplo1.VISITOR.CC as DependentCC, exemplo1.VISITOR.Address as DependentAddress, exemplo1.VISITOR.Phone as DependentPhone from exemplo1.PATIENT 
->>>>>>> 3c1464f4d37f59a7f2b5fa3647374ada794d4cdb
 		JOIN exemplo1.BED on E_BedNumber = BedNumber
 		JOIN exemplo1.BEDROOM on E_RoomNumber = RoomNumber
 		FULL OUTER JOIN exemplo1.DEPENDENT on E_PatientNIF = NIF
