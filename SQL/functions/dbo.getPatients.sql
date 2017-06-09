18017396069eadde48d0d06ea6807357738fc630
Pedro Santos <pedroamaralsantos@ua.pt>
2017-06-09 21:48:48 +0100

    repeat commit with some changes, put xmla order by in patients and appointments

--- SQL/functions/dbo.getPatients.sql
+++ SQL/functions/dbo.getPatients.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION dbo.getPatients(@PatientNif VARCHAR(9)=NULL,@PatientName VARCHAR(30)=NULL,@Sex varchar(1)=NULL, @authorization BIT=NULL,@RoomNumber INT=NULL,@PhoneNUmber INT=NULL,@Checkout BIT=NULL,@PageNumber INT, @RowsPage INT)
+CREATE FUNCTION dbo.getPatients(@PatientNif VARCHAR(9)=NULL,@PatientName VARCHAR(30)=NULL,@Sex varchar(1)=NULL, @authorization BIT=NULL,@RoomNumber INT=NULL,@PhoneNUmber INT=NULL,@Checkout BIT=NULL,@PageNumber INT, @RowsPage INT, @sortOrder VARCHAR(5),@sortColumn VARCHAR(30))
 RETURNS TABLE
 AS
 	RETURN(
@@ -20,7 +20,55 @@ AS
 				AND
 					(@PhoneNUmber IS NULL OR exemplo1.PATIENT.Phone = @PhoneNUmber)
 				AND
-					(@Checkout IS NOT NULL OR exemplo1.Patient.Check_out IS NULL ) ORDER BY exemplo1.PATIENT.NIF
+					(@Checkout IS NOT NULL OR exemplo1.Patient.Check_out IS NULL ) ORDER BY  
+							case
+							when @sortOrder <> 'ASC' then ''
+							when @sortColumn = 'Name' then exemplo1.PATIENT.Name 
+							end ASC
+						   ,case
+							when @sortOrder <> 'ASC' then ''
+							when @sortColumn = 'Nif' then exemplo1.PATIENT.NIF
+							end ASC
+						   ,case
+							when @sortOrder <> 'ASC' then ''
+							when @sortColumn = 'Check in' then Check_in
+							end ASC
+							,case
+							when @sortOrder <> 'ASC' then ''
+							when @sortColumn = 'Check out' then Check_out
+							end ASC
+							,case
+							when @sortOrder <> 'ASC' then ''
+							when @sortColumn = 'Room Number' then RoomNumber
+							end ASC
+							,case
+							when @sortOrder <> 'ASC' then ''
+							when @sortColumn = 'Bed Number' then BedNumber 
+							end DESC
+						   ,case
+							when @sortOrder <> 'DESC' then ''
+							when @sortColumn = 'Name' then exemplo1.PATIENT.Name 
+							end DESC
+						   ,case
+							when @sortOrder <> 'DESC' then ''
+							when @sortColumn = 'Nif' then exemplo1.PATIENT.NIF
+							end DESC
+						   ,case
+							when @sortOrder <> 'DESC' then ''
+							when @sortColumn = 'Check in' then Check_in
+							end DESC
+							,case
+							when @sortOrder <> 'DESC' then ''
+							when @sortColumn = 'Check out' then Check_out
+							end DESC
+							,case
+							when @sortOrder <> 'DESC' then ''
+							when @sortColumn = 'Room Number' then RoomNumber
+							end DESC
+							,case
+							when @sortOrder <> 'DESC' then ''
+							when @sortColumn = 'Bed Number' then BedNumber 
+							end DESC
 			OFFSET ((@PageNumber - 1) * @RowsPage) ROWS
 			FETCH NEXT @RowsPage ROWS ONLY
 		  )		
