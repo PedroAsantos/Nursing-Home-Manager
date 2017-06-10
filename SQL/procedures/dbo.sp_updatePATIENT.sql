ec1eff874488a55855cacd44f76758c0447d1869
Pedro Santos <pedroamaralsantos@ua.pt>
2017-06-10 16:22:15 +0100

    add final update

--- SQL/procedures/dbo.sp_updatePATIENT.sql
+++ SQL/procedures/dbo.sp_updatePATIENT.sql
@@ -14,7 +14,9 @@ AS
 BEGIN
 
 	
-			UPDATE LarIdosos.PATIENT
+		BEGIN TRANSACTION;
+		BEGIN TRY
+			UPDATE exemplo1.PATIENT
 			SET		NIF = @NIF,
 					Name = @Name,
 					Sex = @Sex,
@@ -27,5 +29,21 @@ BEGIN
 					Entry_Date = @Entry_Date,
 					Exit_Date = @Exit_Date
 			WHERE NIF=@NIF
+		END TRY
+
+		BEGIN CATCH
+		SELECT 
+			ERROR_NUMBER() AS ErrorNumber
+			,ERROR_SEVERITY() AS ErrorSeverity
+			,ERROR_STATE() AS ErrorState
+			,ERROR_PROCEDURE() AS ErrorProcedure
+			,ERROR_LINE() AS ErrorLine
+			,ERROR_MESSAGE() AS ErrorMessage;
+
+		IF @@TRANCOUNT > 0
+			ROLLBACK TRANSACTION;
+	END CATCH;
+	IF @@TRANCOUNT > 0
+		COMMIT TRANSACTION;
 		
 END
